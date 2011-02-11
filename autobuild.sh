64a4df41951b8c6b33f89a9b081adb5df1c9e721
Daniel P. Berrange <berrange@redhat.com>
2011-02-11 11:17:52 +0000

    Save 'html' dir before building RPM since it will clean it out
    * autobuild.sh: Save html

--- autobuild.sh
+++ autobuild.sh
@@ -5,9 +5,11 @@ set -v
 
 contentdir=$AUTOBUILD_INSTALL_ROOT/share/publican/Common_Content
 
+rm -rf html
 make clean contentdir=$contentdir
 
 make contentdir=$contentdir
+mv tmp/en-US/html html
 
 if [ -x /usr/bin/rpmbuild ]
 then
